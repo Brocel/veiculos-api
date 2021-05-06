faef91ea1cf263a0e58b7fd1af9d98013c2d4454
Brocel <geenens.prof@gmail.com>
2021-05-06 18:45:17 -0300

    Edit : correct error in factory

--- spec/factories/veiculos.rb
+++ spec/factories/veiculos.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :veiculo do
     marca { Faker::Lorem.word }
     veiculo { Faker::Lorem.word }
-    ano { Faker::Number.number(4) }
+    ano { Faker::Number.number(digits:4) }
     descricao { Faker::Lorem.word }
     vendido { Faker::Boolean.boolean }
 
